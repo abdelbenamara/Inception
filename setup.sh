46480db228fac082efc3e117ab0cf51bc1764e13
Abdel Benamara <abdelb.contact@gmail.com>
2024-10-23 19:05:41 +0200

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+login=$1
+
 main() {
-    apt update
+    if [ -z "$login" ]
+    then
+        echo "Usage: setup.sh login"
+
+        exit 2
+    fi
 
     # Setup Python 3 virtual environment for Ansible playbook
+    apt update
     apt install -y python3 python3-pip python3-venv
-    python3 -m venv inception-venv
-    source inception-venv/bin/activate
+    python3 -m venv venv
+    source venv/bin/activate
     pip install --upgrade pip && pip install --upgrade setuptools wheel && pip install ansible-core
     ansible-galaxy collection install community.general
     ansible-galaxy collection install community.crypto
 
     # Setup inception project
     wget https://raw.githubusercontent.com/abdelbenamara/Inception/main/inception-setup-debian.yml
-    ansible-playbook inception-setup-debian.yml
+    ansible-playbook inception-setup-debian.yml --extra-vars "login=$login"
 }
 
 main

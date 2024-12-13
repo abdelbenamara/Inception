dd0f3f504eba1ef8d770a1ad0eb8bb24b8823c25
Anes BENAMARA <abenamar@student.42.fr>
2024-12-13 22:59:15 +0100

    Updated wordpress

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -3,32 +3,25 @@
 set -eo pipefail
 
 main() {
-	wpEnvs=( "${!WORDPRESS_@}" )
-	
-	if [ ! -s "wp-config.php" ] && [ "${#wpEnvs[@]}" -gt 0 ]; then
-		if [ -s "wp-config-docker.php" ]; then
-			echo >&2 "No 'wp-config.php' found in $PWD, but 'WORDPRESS_...'" \
-				"variables supplied; copying '$wpConfigDocker' (${wpEnvs[*]})"
-			# using "awk" to replace all instances of 
-			# "put your unique phrase here" with a properly unique string 
-			# (for AUTH_KEY and friends to have safe defaults 
-			# if they aren't specified with environment variables)
-			awk '
-				/put your unique phrase here/ {
-					cmd = "head -c1m /dev/urandom | sha1sum | cut -d\\  -f1"
-					cmd | getline str
-					close(cmd)
-					gsub("put your unique phrase here", str)
-				}
-				{ print }
-			' "$wpConfigDocker" > wp-config.php
-			if [ "$(id -u)" = '0' ]; then
-				# attempt to ensure that wp-config.php is owned by the run user
-				# could be on a filesystem that doesn't allow chown 
-				# (like some NFS setups)
-				chown "www-data:www-data" wp-config.php || true
+	if [ - "/var/www/html/wp-config.php" ]; then
+		echo "wp-config.php already present, skipping creation"
+	else
+		echo "wp-config.php not found, creating...."
+
+		for var in DB_HOST DB_NAME DB_USER DB_PASSWORD_FILE; do
+			eval wp_var="\$WORDPRESS_${var}"
+
+			if [ -z "$wp_var" ]; then
+				echo "Undefined variable WORDPRESS_"$var >&2
+				exit 1
 			fi
-		fi
+		done
+
+		/usr/local/bin/wp config create \
+			--dbhost="$WORDPRESS_DB_HOST" \
+			--dbname="$WORDPRESS_DB_NAME" \
+			--dbuser="$WORDPRESS_DB_USER" \
+			--prompt=dbpass < $WORDPRESS_DB_PASSWORD_FILE
 	fi
 
 	exec "$@"

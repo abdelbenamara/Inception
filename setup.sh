5deef670f304bca891e08adf02e0fca1510bd14c
Anes BENAMARA <abenamar@student.42.fr>
2024-12-13 18:46:55 +0100

    Updated setup

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@
 
 main() {
     # Setup Python 3 virtual environment for Ansible playbook
-    apt update
-    apt install -y python3 python3-pip python3-venv
+    sudo apt update
+    sudo apt install -y python3 python3-pip python3-venv
     python3 -m venv venv
     source venv/bin/activate
     pip install --upgrade pip \
         && pip install --upgrade setuptools wheel \
-        && pip install --upgrade ansible-core passlib
+        && pip install --upgrade ansible-core passlib \
         && ansible-galaxy collection install --upgrade \
             community.general community.crypto
 
     # Setup inception project
-    wget https://raw.githubusercontent.com/abdelbenamara/\
-    Inception/main/inception-setup-ubuntu.yml \
+    wget "https://raw.githubusercontent.com/abdelbenamara/\
+Inception/main/inception-setup-ubuntu.yml" \
         && ansible-playbook inception-setup-ubuntu.yml --ask-become-pass
 }
 

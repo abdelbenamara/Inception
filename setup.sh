29ae7a2fc044c60cd64beadcddfc8c37b1650d46
Abdel Benamara <abdelb.contact@gmail.com>
2024-12-18 23:29:17 +0100

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-main() {
-    # Setup Python 3 virtual environment for Ansible playbook
-    sudo apt update
-    sudo apt install -y python3 python3-pip python3-venv
-    python3 -m venv venv
-    source venv/bin/activate
-    pip install --upgrade pip \
+set -eo pipefail
+
+main() { 
+    sudo apt update \
+        && sudo apt install -y \
+            python3 python3-pip python3-venv \
+        && python3 -m venv venv \
+        && source venv/bin/activate \
+        && pip install --upgrade pip \
         && pip install --upgrade setuptools wheel \
         && pip install --upgrade ansible-core passlib \
         && ansible-galaxy collection install --upgrade \
-            community.general community.crypto
-
-    # Setup inception project
-    wget "https://raw.githubusercontent.com/abdelbenamara/\
+            community.general community.crypto \
+        && wget "https://raw.githubusercontent.com/abdelbenamara/\
 Inception/main/inception-setup-ubuntu.yml" \
         && ansible-playbook inception-setup-ubuntu.yml --ask-become-pass
 }

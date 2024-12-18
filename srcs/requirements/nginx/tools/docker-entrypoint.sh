e74ea1defe5f1dd266f4298f0b5a64cd805efd98
Abdel Benamara <abdelb.contact@gmail.com>
2024-12-18 23:28:53 +0100

    Updated nginx

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -6,23 +6,27 @@ ME=$(basename "$0")
 
 auto_envsubst() {
 	local template_dir="${NGINX_ENVSUBST_TEMPLATE_DIR:-/etc/nginx/templates}"
-	local suffix="${NGINX_ENVSUBST_TEMPLATE_SUFFIX:-.template}"
 	local output_dir="${NGINX_ENVSUBST_OUTPUT_DIR:-/etc/nginx/http.d}"
-
-	local template defined_envs relative_path output_path subdir
-	defined_envs=$(printf '${%s} ' $(awk "END { for (name in ENVIRON) \
-		{ print ( name ~ /${filter}/ ) ? name : \"\" } }" < /dev/null ))
+	local suffix="${NGINX_ENVSUBST_TEMPLATE_SUFFIX:-.template}"
+	
 	[ -d "$template_dir" ] || return 0
+
 	if [ ! -w "$output_dir" ]; then
-    	echo "$ME: ERROR: $template_dir exists, but $output_dir is not writable"
-    	return 0
+    	echo "$ME: ERROR: $template_dir exists, but" \
+			"$output_dir is not writable" >&2
+    	
+		return 1
   	fi
-		find "$template_dir" -follow -type f -name "*$suffix" -print \
-			| while read -r template; do
+
+	local defined_envs="$(printf '${%s} ' $(env | cut -d'=' -f1))"
+	local template relative_path output_path
+
+	find "$template_dir" -follow -type f -name "*$suffix" -print \
+		| while read -r template; do
 		relative_path="${template#"$template_dir/"}"
 		output_path="$output_dir/${relative_path%"$suffix"}"
-		subdir=$(dirname "$relative_path")
-		mkdir -p "$output_dir/$subdir"
+
+		mkdir -p "$output_dir/$(dirname $relative_path)"
 		echo "$ME: Running envsubst on $template to $output_path"
 		envsubst "$defined_envs" < "$template" > "$output_path"
 	done

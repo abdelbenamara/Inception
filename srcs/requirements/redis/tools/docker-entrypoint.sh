01bfc4f199f8b7329f57dbb3c16a7e6d4fbe4d4d
Abdel Benamara <abdelb.contact@gmail.com>
2024-12-19 13:03:59 +0100

    Updated redis

---
+++ srcs/requirements/redis/tools/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eo pipefail
+
+main() {
+    if ! redis-cli ping > /dev/null 2>&1; then
+        for var in DEFAULT_PASSWORD_FILE \
+            USER PASSWORD_FILE; do
+            eval redis_var="\$REDIS_"$var
+
+            if [ -z "$redis_var" ]; then
+                echo "Undefined variable REDIS_"$var >&2
+                exit 1
+            fi
+        done
+
+        set -- "$@" "--requirepass" "$(cat $REDIS_DEFAULT_PASSWORD_FILE)" \
+            "--user" "$REDIS_USER" "on" "allkeys" "allcommands" "allchannels" \
+            ">$(cat $REDIS_PASSWORD_FILE)"
+    fi
+
+    exec "$@"
+}
+
+main "$@"
+
+exit $?

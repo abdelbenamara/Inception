6363fdba7c2b91b43a0cfbd8916954e85ae8d13b
Abdel Benamara <abdelb.contact@gmail.com>
2024-12-19 13:04:01 +0100

    Updated wordpress

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,54 +9,58 @@ _error() {
 
 main() {
 	if [ ! -s "/var/www/html/wp-config.php" ]; then
-		for var in DB_HOST DB_NAME DB_USER DB_PASSWORD_FILE URL TITLE \
-			ADMIN_USER ADMIN_EMAIL ADMIN_PASSWORD_FILE \
+		local var
+
+		for var in DB_HOST DB_NAME DB_USER DB_PASSWORD_FILE \
+			REDIS_USER REDIS_PASSWORD_FILE \
+			URL TITLE ADMIN_USER ADMIN_EMAIL ADMIN_PASSWORD_FILE \
 			USER_LOGIN USER_EMAIL USER_PASSWORD_FILE; do
-			eval wp_var="\$WORDPRESS_${var}"
-			
+			eval wp_var="\$WORDPRESS_"$var
+
 			if [ -z "$wp_var" ]; then
 				_error "Undefined variable WORDPRESS_"$var
 			fi
 		done
 
-		if wp core download > /dev/null \
-			&& chown -R www-data:www-data /var/www/html \
-			&& chmod 1777 /var/www/html; then
-			echo "WordPress downloaded successfully"
-		else
-			_error "Failed to download WordPress"
-		fi
-
-		if wp config create --dbhost="$WORDPRESS_DB_HOST" \
+		if wp core download \
+			&& wp config create --dbhost="$WORDPRESS_DB_HOST" \
 			--dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" \
 			--prompt=dbpass < $WORDPRESS_DB_PASSWORD_FILE > /dev/null; then
-			echo "wp-config.php file created successfully"
+			echo "WordPress wp-config.php file created successfully"
 		else
-			_error "Failed to create wp-config.php file"
+			_error "Failed to create WordPress wp-config.php file"
 		fi
 
-		if wp config set WP_REDIS_HOST "redis" > /dev/null \
-			&& wp config set WP_REDIS_PORT "6379" > /dev/null; then
-			echo "Redis configured successfully"
+		if wp config set FORCE_SSL_ADMIN "true" --raw \
+			&& wp config set WP_REDIS_HOST "redis" > /dev/null \
+			&& wp config set WP_REDIS_PORT "6379" --raw > /dev/null \
+			&& wp config set WP_REDIS_PASSWORD \
+		"['$WORDPRESS_REDIS_USER', '$(cat $WORDPRESS_REDIS_PASSWORD_FILE)']" \
+				--raw > /dev/null; then
+			echo "WordPress configured successfully"
 		else
-			_error "Failed to configure Redis"
+			_error "Failed to configure WordPress"
 		fi
 
 		if wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE \
 			--admin_user=$WORDPRESS_ADMIN_USER --prompt=admin_password \
 			--admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email \
-			< $WORDPRESS_ADMIN_PASSWORD_FILE > /dev/null; then
+			< $WORDPRESS_ADMIN_PASSWORD_FILE > /dev/null \
+			&& chown -R www-data:www-data /var/www/html \
+			&& chmod 1777 /var/www/html; then
 			echo "WordPress installed successfully"
 		else
 			_error "Failed to install WordPress"
 		fi
 
-		if wp plugin install redis-cache --activate > /dev/null \
-			&& wp redis enable > /dev/null; then
-			echo "Redis plugin installed, activated and enabled successfully"
-		else
-			_error "Failed to install, activate and enable Redis plugin"
-		fi
+		echo "Replacing http scheme with https within the WordPress database"
+		wp search-replace "http://$WORDPRESS_URL" "https://$WORDPRESS_URL" \
+			--skip-columns=guid \
+			&& wp theme install twentytwentytwo --activate \
+			&& wp theme delete --all \
+			&& wp plugin uninstall --all \
+			&& wp plugin install redis-cache --activate \
+			&& wp redis enable
 
 		if wp user create $WORDPRESS_USER_LOGIN $WORDPRESS_USER_EMAIL \
 			--role=contributor \
@@ -66,7 +70,7 @@ main() {
 			_error "Failed to create WordPress user"
 		fi
 	fi
-	
+
 	exec "$@"
 }
 
